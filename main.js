7e4870f38f06881279d9259c0e28292e1e1f3054
2anchez <crsanchez10@gmail.com>
2021-03-03 22:08:32 -0500

    Updated portfolio layout

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ class TypeWriter {
       this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
   
       // Initial Type Speed
-      let typeSpeed = 300;
+      let typeSpeed = 100;
   
       if(this.isDeleting) {
         typeSpeed /= 2;
@@ -73,3 +73,9 @@ class TypeWriter {
     var blur = document.getElementById('popup');
     popup.classList.toggle('active')
   }
+
+  // Add sticky to secondary navigation bar
+  window.addEventListener("scroll", function(){
+    var aside = document.querySelector("aside");
+    aside.classList.toggle("sticky", window.scrollY > 700);
+  })
